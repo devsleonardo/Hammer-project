6b50363e3aa3f4b55bde8e8cddbfae47d468b2ee
devsleonardo <usr.leonardo@gmail.com>
2022-01-11 16:51:57 -0300

    0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private toastrService: ToastrService) {}
-  text: string = '';
   title = 'ProjectHammer';
-
-  public getClick(): void {
-    this.toastrService.error('string');
-  }
 }
